fed295c6cf835f162421e6e4a4a59b6796c537b8
Daniel Brooks <bass_rock@me.com>
2021-09-19 17:33:35 -0700

    fix(structure): adding in structure set point

--- src/models/structure.ts
+++ src/models/structure.ts
@@ -1,43 +1,47 @@
-import {Model} from './model';
+import { Model } from './model';
 
 export enum StructureHeatCoolMode {
-    OFF = 'float', //for some reason flair calls off float? Maybe its "floating..." around.
-    COOL = 'cool',
-    HEAT = 'heat',
-    AUTO = 'auto'
+  OFF = 'float', //for some reason flair calls off float? Maybe its "floating..." around.
+  COOL = 'cool',
+  HEAT = 'heat',
+  AUTO = 'auto',
 }
 
 export enum FlairMode {
-    MANUAL = 'manual',
-    AUTO = 'auto'
+  MANUAL = 'manual',
+  AUTO = 'auto'
 }
 
 export class Structure extends Model {
-    public static type = 'structures'
+  public static type = 'structures';
 
-    updatedAt: Date = new Date();
-    createdAt: Date = new Date();
-    isActive = false;
-    home = false;
+  updatedAt: Date = new Date();
+  createdAt: Date = new Date();
+  isActive = false;
+  home = false;
 
-    structureHeatCoolMode: StructureHeatCoolMode = StructureHeatCoolMode.COOL;
-    mode : FlairMode = FlairMode.AUTO;
+  structureHeatCoolMode: StructureHeatCoolMode = StructureHeatCoolMode.COOL;
+  structureHeatCoolModeCalculated?: StructureHeatCoolMode;
+  setPointTemperatureC = 0;
+  mode: FlairMode = FlairMode.AUTO;
 
-    public fromJSON(data: any): Structure {
-      this.name = data.attributes.name;
-      this.createdAt = new Date(data.attributes['created-at']);
-      this.updatedAt = new Date(data.attributes['updated-at']);
+  public fromJSON(data: any): Structure {
+    this.name = data.attributes.name;
+    this.createdAt = new Date(data.attributes['created-at']);
+    this.updatedAt = new Date(data.attributes['updated-at']);
 
-      this.isActive = data.attributes['is-active'];
-      this.home = data.attributes.home;
-      this.structureHeatCoolMode = data.attributes['structure-heat-cool-mode'];
-      this.mode = data.attributes.mode;
+    this.isActive = data.attributes['is-active'];
+    this.home = data.attributes.home;
+    this.structureHeatCoolMode = data.attributes['structure-heat-cool-mode'];
+    this.structureHeatCoolModeCalculated = data.attributes['structure-heat-cool-mode-calculated'] === null ? undefined : data.attributes['structure-heat-cool-mode-calculated'];
+    this.setPointTemperatureC = data.attributes['set-point-temperature-c'];
+    this.mode = data.attributes.mode;
 
-      this.id = data.id;
-      return this;
-    }
+    this.id = data.id;
+    return this;
+  }
 
-    public isPrimaryHome(): boolean {
-      return this.home;
-    }
+  public isPrimaryHome(): boolean {
+    return this.home;
+  }
 }
